196a5a72b43a8f1eacd9f1c63eb63b1db53cb3ad
Aleksey Poddubnyi <lex_rs@mail.ru>
2026-03-11 11:06:51 +0300

    Initial Commit

---
+++ WeatherApp_Test_Power/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp_Test_Power
+//
+//  Created by Алексей Поддубный on 11.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
